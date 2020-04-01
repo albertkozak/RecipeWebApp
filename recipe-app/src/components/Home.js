9809c13404021f4a6fd212742b3cf7bfdb534fa3
Sara <sbanaeirad@my.bcit.ca>
2020-03-31 17:32:35 -0700

    create register

---
+++ recipe-app/src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Recipe App</h1>
+      </div>
+    );
+  }
+}
+export default Home;
---
+++ recipe-app/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <a href="/">Home</a>
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+      </nav>
+    );
+  }
+}
