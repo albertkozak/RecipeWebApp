d9e6ec326ca59f978c2e1159b0cbff1b1d34e684
Sara <sbanaeirad@my.bcit.ca>
2020-03-31 18:03:41 -0700

    first successful creation

--- recipe-app/src/components/authentication/Register.js
+++ recipe-app/src/components/authentication/Register.js
@@ -1,116 +1,100 @@
-﻿﻿import React, { Component } from './node_modules/react';
+﻿﻿import React, { Component } from "react";
 import FormErrors from "../FormErrors";
 import Validation from "../utilities/Validation";
 
-
-class Register extends React.Component{
+class Register extends React.Component {
   constructor() {
     super();
     this.state = {
-            username = "",
-            email = "",
-            password = "",
-            confirmpassword = "",
-            errors: {
-              blankfield: false,
-              matchedpassword: false
-            }
-        }
+      username: "",
+      email: "",
+      password: "",
+      confirmpassword: "",
+      errors: {
+        blankfield: false,
+        matchedpassword: false
+      }
+    };
 
-        this.username = this.username.bind(this);
-        this.email = this.email.bind(this);
-        this.password = this.password.bind(this);
-        this.confirmpassword = this.confirmpassword.bind(this);
-    }
-
-    username(event){
-        this.setState({username:event.target.value})
-    }
-    email(event){
-        this.setState({email:event.target.value})
-    }
-    password(event){
-        this.setState({password:event.target.value})
-    }
-    confirmpassword(event){
-      this.setState({confirmpassword:event.target.value})
+    this.handleRegister = this.handleRegister.bind(this);
   }
 
-handleRegister(event){
-    console.log(email)
+  handleRegister(event) {
+    console.log(this.set.email);
     //fetch api goes here
-}
+  }
 
-clearErrors = () => {
-  this.setState({
-    errors: {
-      blankfield: false,
-      matchedpassword: false
-    }
-  });
-}
+  clearErrors = () => {
+    this.setState({
+      errors: {
+        blankfield: false,
+        matchedpassword: false
+      }
+    });
+  };
 
-render() {
+  render() {
     return (
-        <div className="reg-form">
-          <h1>Register</h1>
-          <FormErrors formerrors={this.state.errors} />
-          <form onSubmit={this.handleRegister}>
-            <div className="field">
-                <input 
-                  className="input" 
-                  type="text"
-                  id="username"
-                  placeholder="Enter username"
-                  onChange={this.username}
-                />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-user"></i>
-                </span>
-            </div>
-            <div className="field">
-                <input 
-                  className="input" 
-                  type="email"
-                  id="email"
-                  placeholder="Enter email"
-                  onChange={this.email}
-                />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-envelope"></i>
-                </span>
-            </div>
-            <div className="field">
-                <input 
-                  className="input" 
-                  type="password"
-                  id="password"
-                  placeholder="Password"
-                  onChange={this.password}
-                />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-lock"></i>
-                </span>
-            </div>
-            <div className="field">
-                <input 
-                  className="input" 
-                  type="password"
-                  id="confirmpassword"
-                  placeholder="Confirm password"
-                  onChange={this.confirmPassword}
-                />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-lock"></i>
-                </span>
-            </div>
-            <div className="field">
-              <p className="control">
-                <button className="button is-success">Register</button>
-              </p>
-            </div>
-          </form>
-        </div>
+      <div className="reg-form">
+        <h1>Register</h1>
+        <FormErrors formerrors={this.state.errors} />
+        <form onSubmit={this.handleRegister}>
+          <div className="field">
+            <input
+              className="input"
+              type="text"
+              id="username"
+              placeholder="Enter username"
+              value={this.state.username}
+            />
+            <span className="icon is-small is-left">
+              <i className="fas fa-user"></i>
+            </span>
+          </div>
+          <div className="field">
+            <input
+              className="input"
+              type="email"
+              id="email"
+              placeholder="Enter email"
+              value={this.state.email}
+              ref={emailInput => (this.email = emailInput)}
+            />
+            <span className="icon is-small is-left">
+              <i className="fas fa-envelope"></i>
+            </span>
+          </div>
+          <div className="field">
+            <input
+              className="input"
+              type="password"
+              id="password"
+              placeholder="Password"
+              value={this.state.password}
+            />
+            <span className="icon is-small is-left">
+              <i className="fas fa-lock"></i>
+            </span>
+          </div>
+          <div className="field">
+            <input
+              className="input"
+              type="password"
+              id="confirmpassword"
+              placeholder="Confirm password"
+              value={this.state.confirmpassword}
+            />
+            <span className="icon is-small is-left">
+              <i className="fas fa-lock"></i>
+            </span>
+          </div>
+          <div className="field">
+            <p className="control">
+              <button className="button is-success">Register</button>
+            </p>
+          </div>
+        </form>
+      </div>
     );
   }
 }
