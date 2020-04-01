9fc53acff93ace6ac9810ffe732f99f299babd8e
Albert Kozak <albert.kozak@live.com>
2020-03-31 22:39:44 -0700

    Created Recipe Component

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -6,9 +6,10 @@ const App = () => {
   // Wasted over an hour attempting to hide the APP ID + KEY in .env but kept running into a CORS error (I'll obtain a new ID + KEY later once resolved)
   // const APP_ID = process.env.APP_ID;
   // const APP_KEY = process.env.APP_KEY;
-
   const APP_ID = "8cd9f53a";
   const APP_KEY = "6d496d09b99d1945ce27a655b53ae9f4";
+
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     getRecipes();
@@ -19,7 +20,7 @@ const App = () => {
       `https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json();
-    console.log(data);
+    setRecipes(data.hits);
   };
 
   return (
