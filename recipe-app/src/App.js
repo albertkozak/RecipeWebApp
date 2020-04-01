b0dcf7921cfaec0eb09ffa5d8d98e9bc618f8b87
Albert Kozak <albert.kozak@live.com>
2020-03-31 22:28:57 -0700

    Succesfully connected to API - Tested Connection

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,26 +1,37 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import Register from "./components/authentication/Register";
+//require("dotenv").config();
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <div>
-            <Navbar />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/register" component={Register} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
+const App = () => {
+  // Wasted over an hour attempting to hide the APP ID + KEY in .env but kept running into a CORS error (I'll obtain a new ID + KEY later once resolved)
+  // const APP_ID = process.env.APP_ID;
+  // const APP_KEY = process.env.APP_KEY;
+
+  const APP_ID = "8cd9f53a";
+  const APP_KEY = "6d496d09b99d1945ce27a655b53ae9f4";
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
-  }
-}
+    const data = await response.json();
+    console.log(data);
+  };
+
+  return (
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default App;
