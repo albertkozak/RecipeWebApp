d893f627595a4a81b7d51dcc88f97f149fdf6deb
Eric Bourne <ericbourne6371@gmail.com>
2020-03-31 17:39:59 -0700

    added FormErrors, got submit to console log the email. waiting on backend to replace BASE_URL so i can recieve JWT

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router,  Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Login from "./components/authentication/Login.js";
@@ -7,15 +7,15 @@ import Login from "./components/authentication/Login.js";
 
 export default function App() {
   return (
-      <Router>
-        <div>
-          <Navbar />
-          {/* <Switch> */}
-            <Route exact path='/' component={Home} />
-            <Route exact path='/Login' component={Login} />
-            {/* <Route exact path='/Register' component={Register} /> */}
-          {/* </Switch> */}
-        </div>
-      </Router>
+    <Router>
+      <div>
+        <Navbar />
+        {/* <Switch> */}
+        <Route exact path='/' component={Home} />
+        <Route exact path='/Login' component={Login} />
+        {/* <Route exact path='/Register' component={Register} /> */}
+        {/* </Switch> */}
+      </div>
+    </Router>
   );
 }
